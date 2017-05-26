01734f45cdcf0d496a25852437a10f1373912227
Sam Escolas <samescolas@gmail.com>
2017-05-25 18:42:36 -0700

    Updated header file to reflect changes

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sescolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/14 15:24:45 by sescolas          #+#    #+#             */
-/*   Updated: 2017/05/24 11:13:01 by sescolas         ###   ########.fr       */
+/*   Updated: 2017/05/25 11:31:32 by sescolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ void				ft_memdel(void **ap);
 void				*ft_memmove(void *dst, const void *src, size_t len);
 void				*ft_memset(void *b, int c, size_t len);
 int					ft_numlen(int n);
-void				ft_putchar(char c);
+int					ft_putchar(int c);
 void				ft_putchar_fd(char c, int fd);
 void				ft_putendl(const char *s);
 void				ft_putendl_fd(const char *s, int fd);
